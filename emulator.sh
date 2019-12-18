a0cf7a7d134fdfcb909c433fa3432223ae9f9779
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2019-12-18 20:09:45 +0100

    gitignore update

--- emulator.sh
+++ emulator.sh
@@ -2,5 +2,6 @@
 # run emulator with specified device
 sudo /home/tommaso/Android/Sdk/emulator/emulator @Nexus_5X_API_29_x86
 
+
 # launch application on emulator
 # tns run android --device Nexus_5X_API_29_x86
