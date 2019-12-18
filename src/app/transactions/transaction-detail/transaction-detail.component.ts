e39abfc74ff4da6c506fd9e1a58af3648e2e8ca3
Tommaso Sebastianelli <t.sebastianelli@pluservice.net>
2019-12-18 16:53:26 +0100

    added RadMetadata interfaces

--- src/app/transactions/transaction-detail/transaction-detail.component.ts
+++ src/app/transactions/transaction-detail/transaction-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 import { ITransaction } from '~/app/models/transaction';
+import { RadMetadata } from '~/app/models/rad-form';
 
 @Component({
 	selector: 'ns-transaction-detail',
@@ -9,12 +10,35 @@ import { ITransaction } from '~/app/models/transaction';
 })
 export class TransactionDetailComponent implements OnInit {
 	public model: ITransaction;
+	private metadata: RadMetadata;
 	constructor(private route: ActivatedRoute) {
 
 	}
 
 	ngOnInit() {
 		this.model = this.route.snapshot && this.route.snapshot.data && this.route.snapshot.data.transaction;
+		if(!this.model){
+			// FIXME should be calledfor new transaction creation
+			return;
+		}
 		console.log("transaction-detail.component - transaction: " + JSON.stringify(this.model));
+		this.metadata = <RadMetadata>{
+			commitMode: "Manual",
+			isReadOnly: false,
+			propertyAnnotations: [
+				{
+					name: "categoryName",
+					displayName: "category",
+					readOnly: true,
+					index: 0,
+					valuesProvider: [],
+					validators: []
+				},{
+					name: "amount",
+					displayName: "amount",
+					index: 1
+				},
+			]
+		}
 	}
 }
