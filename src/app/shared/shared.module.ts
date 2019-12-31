288d2f6129e8c149ccc3927ae50e6229d6015453
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2019-12-31 18:39:59 +0100

    added ripple module and added to transaction items

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NgRippleModule } from 'nativescript-ng-ripple';
 import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
 	imports: [
-		NativeScriptCommonModule
+		NativeScriptCommonModule,
+		NgRippleModule
 	],
 	exports: [
-		TimelineComponent
+		TimelineComponent,
+		NgRippleModule
 	],
 	declarations: [TimelineComponent],
 	providers: []
